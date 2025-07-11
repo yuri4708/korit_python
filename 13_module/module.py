a202964e9e8cc43e071ca13e0b7b91bb63a0b7af
yuri4708 <kiowd@naver.com>
2025-07-11 15:19:19 +0900

    package

---
+++ 13_module/module.py
@@ -0,0 +1,2 @@
+for mypackage import math_utils
+
---
+++ 13_module/mypackage/srring_utils.py
@@ -0,0 +1,8 @@
+def reverse_string(text):
+    return text[::-1]
+
+def to_uppercase(text):
+    return text.upper()
+
+def to_lowercase(text):
+    return text.lower()
